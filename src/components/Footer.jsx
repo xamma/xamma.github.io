b99d832f8ca48126c825f8d53aa9f07c129234a7
Xamma <Xam93@live.de>
2025-07-09 16:02:39 +0200

    Favicon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import bgFooter from '../assets/bg-footer.jpg';
 
 export default function Footer() {
   return (
@@ -7,3 +8,20 @@ export default function Footer() {
     </footer>
   );
 }
+
+// export default function Footer() {
+//   return (
+//     <footer
+//       className="text-white py-6 text-center bg-gray-800 relative"
+//       style={{
+//         backgroundImage: `url(${bgFooter})`,
+//         backgroundSize: 'cover',
+//         backgroundPosition: 'center',
+//       }}
+//     >
+//       <div className="bg-black bg-opacity-60 py-6">
+//         <p>&copy; {new Date().getFullYear()} John Doe. All rights reserved.</p>
+//       </div>
+//     </footer>
+//   );
+// }
